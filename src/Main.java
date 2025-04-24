d0cd8c328cc70fd8dcf1e3aab3643b41cc0c5689
Vivek kumar <vivekkumarse@gmail.com>
2025-04-24 21:12:56 +0530

    deposit and other details added

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,15 @@
 public class Main {
     public static void main(String[] args) {
          Account vivekAccount = new Account();
+
+         vivekAccount.setNumber("12345");
+         vivekAccount.setBalance(5000.0);
+         vivekAccount.setCustomerName("Vivek kumar");
+         vivekAccount.setCustomerEmail("vivekkumar@gmail.com");
+         vivekAccount.setCustomerPhone("919956565656");
+
+         vivekAccount.depositFunds(200.0);
          vivekAccount.withdrawFunds(100.0);
+
     }
 }
